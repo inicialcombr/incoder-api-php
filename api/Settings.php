8c6e11428f3a1330919fd16a0f9e76be81799cb1
Fabiano Lima <facodeli@gmail.com>
2017-12-22 00:40:20 -0200

    first commit

---
+++ api/Settings.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Settings
+ */
+class Settings
+{
+	/**
+	 * @return void
+	 */
+	public function __construct()
+	{
+
+	}
+
+	/**
+	 * Base path
+	 * @return string
+	 */
+	public static function getBasePath()
+	{
+		////////////////////////////////////////////////////////////
+		// Absolute path of files that will be accessed by editor //
+		////////////////////////////////////////////////////////////
+
+		return dirname(__FILE__).'/../../';
+	}
+}
---
+++ api/index.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * @see App
+ */
+require 'App.php';
+
+$app = new App();
+
+/////////////////////////////////////////////////////////////
+// API: Navigator ///////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+$app->route('/navigator', function($request, $response) {
+
+	require 'Navigator.php';
+
+	try {
+
+		$path = $request->getParam('path');
+
+		if (!$path) {
+			throw new Exception('Path is required!');
+		}
+
+		$data = Navigator::list($path);
+
+		$response->setData($data);
+		$response->setMessage('Success!');
+		$response->send();
+
+	} catch (Exception $e) {
+
+		$response->setStatus(false);
+		$response->setMessage($e->getMessage());
+		$response->send();
+	}
+});
+
+/////////////////////////////////////////////////////////////
+// API: Editor //////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+$app->route('/editor', function($request, $response) {
+
+	require 'Editor.php';
+
+	try {
+
+		$path 	 = $request->getParam('path');
+		$content = $request->getParam('content');
+
+		if (!$path) {
+			throw new Exception('Path is required!');
+		}
+
+		$data = Editor::file($path, $content);
+
+		if ($data == false) {
+			throw new Exception('File error!');
+		}
+
+		$response->setData($data);
+		$response->setMessage('Success!');
+		$response->send();
+
+	} catch (Exception $e) {
+
+		$response->setStatus(false);
+		$response->setMessage($e->getMessage());
+		$response->send();
+	}
+});
+
+/////////////////////////////////////////////////////////////
+// API: Fallback ////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+$app->route('/', function($request, $response) {
+
+	$data = array (
+		'version' => '1.0.0'
+	);
+
+	$response->setData($data);
+	$response->send();
+});
